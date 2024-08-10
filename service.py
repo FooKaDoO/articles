8de65de905695ced3591c0dd16e26650281330a7
FooKaDoO <mihkelhani@gmail.com>
2024-08-10 03:03:25 +0300

    Optimizations

--- service.py
+++ service.py
@@ -6,6 +6,7 @@ URL = lambda page_number : f"https://jsonmock.hackerrank.com/api/articles?page={
 
 page_number = 0
 articles = pd.DataFrame()
+session = requests.Session()
 
 cols_to_return = []
 cols_to_keep = []
@@ -34,77 +35,75 @@ key: value to be replaced
 value: replacement value
 """
 def set_replace_cols(**kwargs):
-    global replace_cols
+    global replace_cols, extra_cols, cols_to_keep
     replace_cols = kwargs
 
 """
 Gets the total pages count
 """
 def get_total_pages():
-    res = requests.get(URL(1))
-    json = res.json()
+    global session
+    json = (session
+            .get(URL(1))
+            .json())
     return json['total_pages']
 
 """
-Gets the page data on given page number.
-"""
-def get_page(page_number: int):
-    res = requests.get(URL(page_number))
-    json = res.json()
-    return json['data']
-
-"""
-Increments the page number, gets the data from that page
-and appends the cleaned up data to the articles dataframe.
-"""
-def get_next_page():
-    global page_number, articles
-    page_number += 1
-
-    next_page_data = get_page(page_number)
-    df_next_page_data = pd.json_normalize(next_page_data)
-    df_next_page_data = clean_up_data(df_next_page_data)
-    
-    articles = pd.concat([articles, df_next_page_data], 
-                         ignore_index = True)
-
-"""
-Cleans up dataframe by values 
+Cleans up DataFrame by values 
 cols_to_return, cols_to_keep, replace_cols.
 """
 def clean_up_data(df: pd.DataFrame):
     global cols_to_return, cols_to_keep, replace_cols
     
-    df = df.replace(r'^\s*$', np.nan, regex=True)
-
-    for col in cols_to_keep:
-        if col not in df.columns:
-            df[col] = np.nan
+    df[np.setdiff1d(cols_to_keep + list(replace_cols.values()),
+                     df.columns)] = pd.NA
     
-    for col, replace_col in replace_cols.items():
-        if replace_col in df.columns:
-            df[col] = df[col].fillna(value=df[replace_col])
-    
-    df = df[cols_to_keep]
-    df = df.dropna(subset=cols_to_return)
-
-    return df
+    for col1, col2 in replace_cols.items():
+        df[col1] = (df[col1]
+                    .replace(r'^\s*$', pd.NA, regex=True)
+                    .fillna(df[col2]
+                            .replace(r'^\s*$', pd.NA, regex=True)))
+        df = df.dropna(subset=col1)
+    return df[cols_to_keep]
 
 """
-Checks the total pages number and updates data frame for
-each remaining page.
+Processes page data into a cleaned-up Pandas DataFrame.
 """
-def get_remaining_pages():
-    global page_number
+def process_data(data):
+    global articles
+    articles = pd.concat(
+        [articles, clean_up_data(
+                    pd.json_normalize(data))
+        ], ignore_index = True)
+
+"""
+Checks the total pages number and yields the remaining pages.
+"""
+def get_pages():
+    global page_number, session
     total_pages = get_total_pages()
 
     while page_number < total_pages:
-        get_next_page()
+        page_number += 1
+        yield (session
+                .get(URL(page_number))
+                .json())['data']
+
+"""
+Processes all remaining pages.
+"""
+def process_remaining_pages():
+    remaining_data = []
+    for page in get_pages():
+        remaining_data.append(page)
+
+    if len(remaining_data) > 0:
+        process_data([x for y in remaining_data for x in y])
 
 """
 Using cols_to_return gets the top N entries ordered by ordered_by.
 Does not update data.
-Data has to be updated manually using get_remaining_pages() beforehand.
+Data has to be updated manually using process_remaining_pages() beforehand.
 """
 def get_topN(N: int, ordered_by: str):
     global articles
