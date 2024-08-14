fefed1422f016196dcf337fa3798ef34e6e7d7e1
FooKaDoO <mihkelhani@gmail.com>
2024-08-14 18:44:05 +0300

    Removed business logic from server code

--- app.py
+++ app.py
@@ -1,27 +1,9 @@
 from flask import Flask
 import service
 
-# Initialization
-
-service.set_cols_to_return(
-            'title',
-        )
-service.set_cols_to_keep(
-            'num_comments',
-        )
-service.set_replace_cols(
-            title='story_title',
-        )
-
-how_many_to_keep = 10
-order_by = 'num_comments'
-
-# End of initialization
-
 # REST API endpoints
 app = Flask(__name__)
 
 @app.route("/")
 def get_all_data():
-    service.process_remaining_pages()
-    return service.get_topN(how_many_to_keep, order_by)
+    return service.get_top10_titles()
